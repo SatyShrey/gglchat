225cc8d50bfc5a6030f6fd8e08dbb76078e63c8b
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2024-11-17 13:37:02 +0530

    Update ChatPage.jsx

--- src/ChatPage.jsx
+++ src/ChatPage.jsx
@@ -14,6 +14,7 @@ function ChatPage(){
     useEffect(()=>{
         axios.get(`${url}chats/${user.uid}/${user2.uid}`)
         .then(data=>{if(data.data){setOldMsgArray(data.data)}})
+        .then(()=>myRef.current.scrollIntoView({ behavior: "smooth" }))
         .catch(er=>alert(er.message))
 
         axios.get(url+"users/"+user2.uid).then(data=>setStatus(data.data.status))
